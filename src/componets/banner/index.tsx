0095de5f0bbd273167735ac9134c37b4458166c9
Hubert Prado Ramos <hubert.ramos@escola.pr.gov.br>
2026-03-30 06:52:54 -0300

    Banner corrigido 'Agora ele não é invisivel'

--- src/componets/banner/index.tsx
+++ src/componets/banner/index.tsx
@@ -6,23 +6,25 @@ export default function Banner() {
    <View className='w-full h-36 rounded-2xl mt-5 mb-4'>
         <PagerView style={{flex: 1}} initialPage={0} pageMargin={14}>
             <Pressable
-                className='w-full h-36 rounded-2xl'
+                className='w-full h-36 rounded-2xl relative overflow-hidden'
                 key='1'
                 onPress={() => console.log('Clicou no banner 1')}
-            />
-            <Image
-                source={require('../../assets/feijoada.jpg')}
-                className='w-full h-36 rounded-2xl'
-            />
+            >
+                <Image
+                    source={require('../../assets/imagens/feijoada.jpg')}
+                    className='w-full h-full rounded-2xl'
+                />
+            </Pressable>
             <Pressable
-                className='w-full h-36 rounded-2xl'
+                className='w-full h-36 rounded-2xl relative overflow-hidden'
                 key='2'
                 onPress={() => console.log('Clicou no banner 2')}
-            />
-            <Image
-                source={require('../../assets/pratoFeito.jpg')}
-                className='w-full h-36 rounded-2xl'
-            />
+            >
+                <Image
+                    source={require('../../assets/imagens/pratoFeito.jpg')}
+                    className='w-full h-full rounded-2xl'
+                />
+            </Pressable>
         </PagerView>
    </View>
   );
